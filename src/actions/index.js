f141260e3f0b6681d125c6c8547c4dc4f01c910d
sonamkumari16 <56336173+sonamkumari16@users.noreply.github.com>
2021-08-09 23:38:19 +0530

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,42 @@
+// {
+//     type:'ADD_MOVIES',
+//     movies: [m1,m2,m3]
+// }
+// {
+//     type:: 'DECREASE_COUNT';
+// }
+
+//action types
+export const ADD_MOVIES='ADD_MOVIES';
+export const ADD_FAVOURITE='ADD_FAVOURITE';
+export const REMOVE_FROM_FAVOURITES='REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES='SET_SHOW_FAVOURITES';
+
+
+//action creators
+export function addMovies(movies){
+    return{
+        type:ADD_MOVIES,
+        movies
+    }
+}
+
+export function addFavourite(movie){
+    return{
+        type:ADD_FAVOURITE,
+        movie
+    }
+}
+export function removeFromFavourites(movie){
+    return{
+        type:REMOVE_FROM_FAVOURITES,
+        movie
+    };
+}
+
+export function setShowFavourites(val){
+    return{
+        type: SET_SHOW_FAVOURITES,
+        val
+    }
+}
